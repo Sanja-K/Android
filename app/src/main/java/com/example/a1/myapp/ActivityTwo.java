0d61cc23bc3a025b067cfd22c6226f28ee0175d6
Sanja_K <sanja_19.96@mail.ru>
2017-03-27 22:16:50 +0300

    Добавлены фрагменты

--- app/src/main/java/com/example/a1/myapp/ActivityTwo.java
+++ app/src/main/java/com/example/a1/myapp/ActivityTwo.java
@@ -2,12 +2,40 @@ package com.example.a1.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import static com.example.a1.myapp.R.id.text_v;
 
 public class ActivityTwo extends AppCompatActivity {
+
+
+
+    String[] codeMorse = new String[]{ "01", "0111", "100", "110",
+            "100", "0", "0001", "1100",
+            "00", "0111", "010", "0100",
+            "11", "10", "111", "0110",
+            "010", "000", "1", "001",
+            "0010", "0000", "1010",
+            "0001", "1111", "1101",
+            "1011", "1001", "00100",
+            "0011", "0101", "01111",
+            "00111", "00011", "00001",
+            "00000", "10000", "11000",
+            "11100", "11110", "11111" };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
+        TextView pTextView;
+        String array_undex=getIntent().getExtras().getString("array_key");
+
+        pTextView = (TextView)findViewById(R.id.text_two);
+
+        pTextView.setText(array_undex);
+
     }
+
+
 }
--- app/src/main/java/com/example/a1/myapp/MainActivity.java
+++ app/src/main/java/com/example/a1/myapp/MainActivity.java
@@ -1,42 +1,35 @@
 package com.example.a1.myapp;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.util.ArrayMap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//import android.util.ArrayMap;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.TableRow;
 import android.widget.TextView;
-import android.view.View.OnClickListener;
 
-import java.sql.Time;
-import java.util.Timer;
-import java.util.TimerTask;
+import static java.util.Arrays.*;
+
+import java.lang.String;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-import java.util.logging.LogRecord;
-import java.util.concurrent.TimeUnit;
-
-import android.app.Activity;
-import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-import static com.example.a1.myapp.R.color.colorAccent;
-import static com.example.a1.myapp.R.color.screenBlack;
-import static com.example.a1.myapp.R.color.screenGreen;
-import static com.example.a1.myapp.R.color.screenWhite;
-import static com.example.a1.myapp.R.color.secondary_text;
-import static com.example.a1.myapp.R.id.text_v;
+import static java.util.Arrays.binarySearch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,12 +39,19 @@ public class MainActivity extends AppCompatActivity {
 
     TextView hTextView;
     Button startButton ;
-    Handler h;
+    Handler handler;
     private EditText editText;
+    String text_input;
 
-    ArrayMap<String, String> arrayMap = new ArrayMap<>();
+    Fregment_flash fregment_flash;
+
+/// что каждый сделал ( что сделал я и что сделал антоха) и почему у нас не получилось
+
+  final   ArrayMap<String, String> arrayMap = new ArrayMap<>();
+
 
     private static final String TAG = "myLogs";
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,15 +60,17 @@ public class MainActivity extends AppCompatActivity {
 
 
         startButton = (Button)findViewById(R.id.button);
-        hTextView = (TextView)findViewById(text_v);
+        hTextView = (TextView)findViewById(R.id.text_v);
         editText = (EditText) findViewById(R.id.editTextActMain);
 
         mRelativeLayout=(RelativeLayout) findViewById(R.id.activity_main) ;
         mTwolinear=(ConstraintLayout) findViewById(R.id.activity_two) ;
 
+        fregment_flash=new Fregment_flash();
+
         arrayMap.put("а", "*--");
         arrayMap.put("б", "-***");
-        arrayMap.put("в", "*-- ");
+        arrayMap.put("в", "*--*");
         arrayMap.put("г", "--*");
         arrayMap.put("д", "-** ");
         arrayMap.put("е", "*");
@@ -113,7 +115,9 @@ public class MainActivity extends AppCompatActivity {
         arrayMap.put(" ", "_");
 
 
-        h=new Handler() {
+
+
+        handler=new Handler() {
 
             public  void  handleMessage(android.os.Message msg){
                 mRelativeLayout.setBackgroundColor(getResources().getColor(msg.what));
@@ -125,30 +129,162 @@ public class MainActivity extends AppCompatActivity {
         startButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
-            public void onClick(View view) {
+            public void onClick(final View view) {
                 Thread t=new Thread(new Runnable() {
                     @Override
                     public void run() {
 
-                    for (int j=0; j<editText.getText().toString().length();j++){
+                        text_input=editText.getText().toString();
+
+                        // получаем экземпляр FragmentTransaction
+                        FragmentManager fragmentManager = getFragmentManager();
+                        FragmentTransaction fragmentTransaction = fragmentManager
+                                .beginTransaction();
+
+                        // добавляем фрагмент
+                        Fregment_flash fregment_flash = new Fregment_flash();
+                        fragmentTransaction.add(R.id.frame, fregment_flash);
+                        fragmentTransaction.commit();
+
+                        Bundle bundle = new Bundle();
+
+                       // String qwe=arrayMap.valueAt(9);
+                       // bundle.putString("key", qwe);
+                       // fregment_flash.setArguments(bundle);
+
+
+                       // bundle.putBoolean("key", arrayMap.containsKey(0));
+
+                   //   Log.d(TAG, "containsKey! "+ arrayMap.containsKey("а"));
+
+                      //  Log.d(TAG, "arrayMap.get! "+ arrayMap.get(String.valueOf(text_input.charAt(0))));
+                      //  Log.d(TAG, "text_input.charAt(0)! "+ text_input);
+
+                        /*if(arrayMap.containsKey(String.valueOf(text_input.charAt(0)))){
+
+                            Log.d(TAG, "arrayMap.get Fragments! "+ arrayMap.get(String.valueOf(text_input.charAt(0))));
+                            Log.d(TAG, "text_input.charAt(0)! "+ text_input);
+                        }*/
+
+                   //     bundle.putString("key", arrayMap.valueAt(1));
+                     //   fregment_flash.setArguments(bundle);
+
+                    //    Log.d(TAG, "text_input.charAt(0)! "+ text_input);
+
+                        /*bundle.putInt("length", text_input.length());
+                        fregment_flash.setArguments(bundle);
+
+                        for(int i=0; i<text_input.length(); i++){
+
+                            String key=String.valueOf(text_input.charAt(i));
+                            if(arrayMap.containsKey(key)){
+
+
+                                bundle.putString("key"+i, arrayMap.get(key));
+                                fregment_flash.setArguments(bundle);
+                                // Log.d(TAG, "text_input.charAt(i)! "+ text_input.charAt(i));
+                                Log.d(TAG, "arrayMap.get! "+ arrayMap.get(key));
+                            }
+
+                        }*/
+
+                        for(int i=0; i<text_input.length(); i++){
+
+                            String key=String.valueOf(text_input.charAt(i));
+                            if(arrayMap.containsKey(key)){
+
+
+                                bundle.putString("key"+i, arrayMap.get(key));
+                                fregment_flash.setArguments(bundle);
+                                // Log.d(TAG, "text_input.charAt(i)! "+ text_input.charAt(i));
+                                Log.d(TAG, "arrayMap.get! "+ arrayMap.get(key));
+                            }
+
+                        }
+
+
+
+
+                      /*  // получаем экземпляр FragmentTransaction
+                        FragmentManager fragmentManager = getFragmentManager();
+                        FragmentTransaction fragmentTransaction = fragmentManager
+                                .beginTransaction();
+
+                        // добавляем фрагмент
+                        Fregment_flash fregment_flash = new Fregment_flash();
+                        fragmentTransaction.add(R.id.frame, fregment_flash);
+                        fragmentTransaction.commit();*/
+
+
+
+                        /*
+                        String qwe="qwerty ";
+                        Bundle bundle = new Bundle();
+                        bundle.putString("key", qwe);
+                        fregment_flash.setArguments(bundle);
+
+                        */
+
+                     //   fragmentTransaction=getFragmentManager().beginTransaction();
+
+
+                    //    fragmentTransaction.add(R.id.frame,fregment_flash);
+                     //   fragmentTransaction.commit();
+
+
+               /*     for(int i=0; i<text_input.length(); i++){
+
+                        if(arrayMap.containsKey(text_input.charAt(i))){
+
+                        }
+                    }
+
+
+                        Set<Map.Entry<String, String>> set=hashmap.entrySet();
+                        Log.d(TAG, "SET! "+ set);
+
+                        if(arrayMap.containsValue("--")){
+
+                            Log.d(TAG, "HACH! value a "+ arrayMap.get("а"));
+                        }
+                        */
+
+
+                       // hashmap.containsValue("а");
+                       // Intent intent = new Intent(MainActivity.this,ActivityTwo.class);
+                       // intent.putExtra("array_key",index);
+                       // Arrays.sort(set);
+
+                      //  int index= Arrays.binarySearch(set,"а");
+
+                       // for(int i=0;i<codeMorse.length;i++) {
+                         //   Log.d(TAG, "codeMorse! " + index);
+                      //  }
+
+                    //hTextView.setText(index);
+
+                       // startActivity(intent);
+                  /*  for (int j=0; j<text_input.length();j++){
+
 
                     for(int g=0; g<arrayMap.size();g++) {
-                        if(String.valueOf(editText.getText().toString().charAt(j)).equals(arrayMap.keyAt(g))){
+
+                        if(String.valueOf(text_input.charAt(j)).equals(arrayMap.keyAt(g))){
 
                             for( int i=0; i<arrayMap.valueAt(g).length();i++){
 
                                 switch (arrayMap.valueAt(g).charAt(i)) {
                                     case '-':{
-                                        h.sendEmptyMessage(screenWhite);
+                                        handler.sendEmptyMessage(screenWhite);
                                         text_output(1000);
-                                        h.sendEmptyMessage(screenBlack);
+                                        handler.sendEmptyMessage(screenBlack);
                                         text_output(500);
                                         break;
                                     }
                                     case '*':{
-                                        h.sendEmptyMessage(screenWhite);
+                                        handler.sendEmptyMessage(screenWhite);
                                         text_output(2000);
-                                        h.sendEmptyMessage(screenBlack);
+                                        handler.sendEmptyMessage(screenBlack);
                                         text_output(500);
                                         break;
                                     }
@@ -159,14 +295,14 @@ public class MainActivity extends AppCompatActivity {
                                     }
 
                                     default:{
-                                        h.sendEmptyMessage(secondary_text);
+                                        handler.sendEmptyMessage(secondary_text);
                                         break;
                                     }
                                 }
                             }
                         }
                     }
-                }
+                }*/
                     }
 
                 });
